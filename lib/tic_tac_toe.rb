bbb189fe729eba7a6581b85eb0c221bc072d7870
John Day <itisjohnday@gmail.com>
2017-09-10 23:55:36 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -98,8 +98,8 @@ def position_taken?(board, index)
   board[index] != " "
 end
 
-def move(board, index)
-    board[index] = current_player(board)
+def move(board, index, token = 'X')
+    board[index] = token
 end
 
 def turn(board)
